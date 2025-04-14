db05301c08ea216e13fcca84a3f4ec7f489296f9
vaachu1993 <triminh22065@gmail.com>
2025-04-14 12:37:01 +0700

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
@@ -82,4 +83,30 @@ const MainLayout = ({ children }) => (
     </>
 );
 
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+>>>>>>> ad2b6d5 (Initialize project using Create React App)
 export default App;
